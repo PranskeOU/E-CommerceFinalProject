17a37144e073966e67b6545d35682847d1595a06
Logan-Manuel <97976110+Logan-Manuel@users.noreply.github.com>
2022-11-21 14:34:48 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,9 +5,10 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Contact Us</title>
   </head>
   <body>
-   
+    <h1>Contact Us</h1>
+  
   </body>
 </html>
