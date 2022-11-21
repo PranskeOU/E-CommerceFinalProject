03d68a0d2b00ec90f012e425daa172a811de9f06
Logan-Manuel <97976110+Logan-Manuel@users.noreply.github.com>
2022-11-21 14:41:32 -0600

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,15 +1,6 @@
 <?php require_once("header.php"); ?>
-<!doctype html>
-
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Contact Us</title>
-  </head>
   <body>
     <h2>Contact Us</h2>
  
   
   </body>
-</html>
