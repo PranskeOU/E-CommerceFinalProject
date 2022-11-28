db2ab452603ae3e024ab3b1dab2fdb586b8c8389
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-11-28 14:18:40 -0600

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,28 @@
     <title>Bootstrap demo</title>
   </head>
   <body>
-   
+   <?php
+    require_once("connection.php");
+    $sql = "SELECT * from customer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["CustomerID"]?></td>
+    <td><?=$row["CustomerName"]?></td>
+    <td><?=$row["CustomerPhone"]?></td>
+    <td><?=$row["CustomerEmail"]?></td>
+    <td><?=$row["CustomerAddress"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
   </body>
 </html>
