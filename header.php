b3d424366e7448107b50f4ae0e7610d0d4ee53af
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-05 13:51:42 -0600

    Update header.php

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>juans's your daddy</title>
+    <title>ManClothes.com</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 
     <nav class="navbar navbar-expand-lg bg-light">
