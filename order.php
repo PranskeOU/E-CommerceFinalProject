0ea7db8e66bc60a900ccd84d5fa2c8bd1d8280fb
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-06 17:54:02 -0600

    Update order.php

--- order.php
+++ order.php
@@ -8,6 +8,12 @@
   margin-left: auto;
   margin-right: auto;
 }
+        body {
+  background-image: url('https://res.cloudinary.com/teepublic/image/private/s--bxj92afB--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1564609475/production/designs/5473319_0.jpg');
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
+}
           h2 {
   display: block;
   margin-left: auto;
