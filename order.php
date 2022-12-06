677e9b1e14fbc9d6eeecb0bed585595ba59d836e
Logan-Manuel <97976110+Logan-Manuel@users.noreply.github.com>
2022-12-06 08:16:21 -0600

    Update order.php

--- order.php
+++ order.php
@@ -1,57 +1,65 @@
-<?php require_once("header.php"); ?>
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<style>
-* {
-  box-sizing: border-box;
-}
-
-.menu {
-  float: left;
-  width: 20%;
-  text-align: center;
-}
-
-.menu a {
-  background-color: #e5e5e5;
-  padding: 8px;
-  margin-top: 7px;
+<!doctype html>
+<html lang="en">
+<head> <?php require_once("header.php"); ?>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+     img {
   display: block;
-  width: 100%;
-  color: black;
+  margin-left: auto;
+  margin-right: auto;
 }
+          h2 {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+        * {
+            box-sizing: border-box;
+        }
 
-.main {
-  float: left;
-  width: 60%;
-  padding: 0 20px;
-}
+        .menu {
+            float: left;
+            width: 20%;
+            text-align: center;
+        }
 
-.right {
-  background-color: #e5e5e5;
-  float: left;
-  width: 20%;
-  padding: 15px;
-  margin-top: 7px;
-  text-align: center;
-}
+            .menu a {
+                background-color: dimgrey;
+                padding: 8px;
+                margin-top: 7px;
+                display: block;
+                width: 100%;
+                color: white;
+            }
 
-@media only screen and (max-width: 620px) {
-  /* For mobile phones: */
-  .menu, .main, .right {
-    width: 100%;
-  }
-}
-</style>
+        .main {
+            float: left;
+            width: 60%;
+            padding: 0 20px;
+        }
+
+        .right {
+            background-color: #e5e5e5;
+            float: left;
+            width: 20%;
+            padding: 15px;
+            margin-top: 7px;
+            text-align: center;
+        }
+
+        @media only screen and (max-width: 620px) {
+            /* For mobile phones: */
+            .menu, .main, .right {
+                width: 100%;
+            }
+        }
+    </style>
 </head>
 <body style="font-family:Verdana;color:#aaaaaa;">
-
-<div style="background-color:#e5e5e5;padding:15px;text-align:center;">
-  <h1>Hello World</h1>
-</div>
-
+    <div style="background-color:#e5e5e5;padding:15px;text-align:center;color: black;">
+        <h1>Man Clothes</h1>
+    </div>
+  
 <div style="overflow:auto">
   <div class="menu">
     <a href="#">Link 1</a>
@@ -60,19 +68,43 @@
     <a href="#">Link 4</a>
   </div>
 
-  <div class="main">
-    <h2>Lorum Ipsum</h2>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-  </div>
+        <div class="main">
+            <h2 id="title">Black Sweatpants</h2>
+            <img id="image" src="https://cdn.shopify.com/s/files/1/0089/7912/0206/products/LOTFEELPJ541_21_-MO_800x.jpg?v=1638346603" width="490" height="490">
+            </div>
+        <div class="right">
+            <h2>About</h2>
+            <p id="price">Price is: $20 </p>
+         <a class="btn btn-primary" href="order.php" role="button">Order</a>
+        </div>
+    </div>
 
-  <div class="right">
-    <h2>About</h2>
-    <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</p>
-  </div>
-</div>
+    <div style="background-color:#A78C59;text-align:center;padding:10px;margin-top:7px;">© copyright JuanNickLogan.oak</div>
 
-<div style="background-color:#e5e5e5;text-align:center;padding:10px;margin-top:7px;">© copyright w3schools.com</div>
 
-</body>
+
+   <?php
+    require_once("connection.php");
+    $sql = "SELECT * from Customer";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+?>
+  <tr>
+    <td><?=$row["CustomerID"]?></td>
+    <td><?=$row["CustomerName"]?></td>
+    <td><?=$row["CustomerPhone"]?></td>
+    <td><?=$row["CustomerEmail"]?></td>
+    <td><?=$row["CustomerAddress"]?></td>
+  </tr>
+<?php
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
+  </body>
 </html>
-
