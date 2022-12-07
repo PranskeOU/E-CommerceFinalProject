dee675e576f1fb3de21915ce00f5bf0d08c6e44c
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-06 20:45:15 -0600

    Update allorders.php

--- allorders.php
+++ allorders.php
@@ -5,7 +5,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
+    <title>Man Clothes</title>
   </head>
   <body>
     <table class="table table-striped">
