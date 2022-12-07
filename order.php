eea0e6e5fee9cd6b6f247516b973950dbc822bf2
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-07 00:56:06 -0600

    Update order.php

--- order.php
+++ order.php
@@ -9,7 +9,8 @@
   margin-right: auto;
 }
         body {
-  background-image: url('https://res.cloudinary.com/teepublic/image/private/s--bxj92afB--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1564609475/production/designs/5473319_0.jpg');
+  /*background-image: url('https://res.cloudinary.com/teepublic/image/private/s--bxj92afB--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1564609475/production/designs/5473319_0.jpg');
+  */
   background-repeat: no-repeat;
   background-attachment: fixed; 
   background-size: 100% 100%;
@@ -93,33 +94,6 @@
          <a class="btn btn-primary" href="order.php" role="button">Confirm Order</a>
         </div>
     </div>
-
     <div style="background-color:#A78C59;text-align:center;padding:10px;margin-top:7px;">© copyright JuanNickLogan.oak</div>
-
-
-
-   <?php
-    require_once("connection.php");
-    $sql = "SELECT * from Customer";
-$result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["CustomerID"]?></td>
-    <td><?=$row["CustomerName"]?></td>
-    <td><?=$row["CustomerPhone"]?></td>
-    <td><?=$row["CustomerEmail"]?></td>
-    <td><?=$row["CustomerAddress"]?></td>
-  </tr>
-<?php
-  }
-} else {
-  echo "0 results";
-}
-$conn->close();
-?>
   </body>
 </html>
