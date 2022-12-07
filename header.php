c5caf756ba3dca9606832c1f31889936034ffb80
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-06 19:52:37 -0600

    Update header.php

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="yourorders.php">Past Orders</a>
+          <a class="nav-link active" aria-current="page" href="allorders.php">Past Orders</a>
         </li>
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
