d20329f3894d8dcf5a904fde413f75902224f23d
Logan-Manuel <97976110+Logan-Manuel@users.noreply.github.com>
2022-12-13 20:34:49 -0600

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,79 +1,99 @@
-<?php require_once("header.php"); ?>
-<!DOCTYPE html>
-<html>
-<head>
-<title> EduCba </title>
-<style>
-li{ display: inline-flex;
-padding: 25px
+<!doctype html>
+<html lang="en">
+<head> <?php require_once("header.php"); ?>
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+     img {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
-section{
-background-color: #607D8B;
-width: 79%;
-position: absolute;
-height: 150px;
+        body {
+  /*background-image: url('https://res.cloudinary.com/teepublic/image/private/s--bxj92afB--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1564609475/production/designs/5473319_0.jpg');
+  */
+  background-repeat: no-repeat;
+  background-attachment: fixed; 
+  background-size: 100% 100%;
 }
-article{
-width: 79%;
-background-color: #607d8bc7;
-position: absolute;
-top: 368px;
-height: 150px;
+          h2 {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
 }
-aside{
-background-color: #607d8bde;
-width: 20%;
-position: absolute;
-left: 80%;
-height: 300px;
-}
-h2 , p {
-text-align: center;
-color: #fff;
-}
-ul{
-text-align: center;
-}
-details {
-margin-top: 330px;
-width: 100%;
-padding-top: 10px;
-padding-bottom: 50px;
-padding-left: 15px;
-background-color: #607d8bdb;
-}
-details p {
-font-size: 18px;
-}
-summary {
-color: white;
-font-size: 22px;
-}
-</style>
-</head>
-<body>
+        * {
+            box-sizing: border-box;
+        }
 
-<div class = "main">
-<section>
-<h2> Section Part </h2>
-<p> Some Text </p>
-</section>
-<article>
-<h2> Article Part</h2>
-<p> Some Detailed Text </p>
-</article>
-<aside>
-<h2> Sidebar Part </h2>
-<p> This will contain static part or anything like advertisement etc. </p>
-</aside>
-</div>
-<details>
-<summary> Summary: click here to show details </summary>
-<p> Details: Upon clicking summary element, details will be shown to the user </p>
-</details>
-<footer style = " height: 100px; background-color:#607D8B; width: 100%; text-align: center;">
-<h3 style = " color: #fff; text-align: center; padding-top: 10px; font-size: 30px; " >Footer Section</h3>
-<p> © Copyright ( Year ) </p>
-</footer>
-</body>
+        .menu {
+            float: left;
+            width: 20%;
+            text-align: center;
+            background: #415665;
+            height: 75px;
+            
+            border-radius: 15px;
+            padding: 20px;
+            font-size: 22px;
+            color: #fff;
+        }
+
+            .menu a {
+                background-color: dimgrey;
+                padding: 8px;
+                margin-top: 7px;
+                display: block;
+                width: 100%;
+                color: white;
+            }
+
+        .main {
+            float: left;
+            width: 60%;
+            padding: 0 20px;
+        }
+
+        .right {
+            background-color: #e5e5e5;
+            float: left;
+            width: 20%;
+            padding: 15px;
+            margin-top: 7px;
+            text-align: center;
+        }
+
+        @media only screen and (max-width: 620px) {
+            /* For mobile phones: */
+            .menu, .main, .right {
+                width: 100%;
+            }
+        }
+    </style>
+</head>
+<body style="font-family:Verdana;color:#aaaaaa;">
+    <div style="background-color:#e5e5e5;padding:15px;text-align:center;color: black;">
+        <h1>Man Clothes</h1>
+    </div>
+  
+<div style="overflow:auto">
+        <div class="menu">
+            <label>Enter a quantity: </label><select id="selectvalue">
+                <option>Small</option>
+                <option>Medium</option>
+                <option>Large</option>
+            </select>
+
+        </div>
+
+        <div class="main">
+            <h2 id="title">Black Sweatpants</h2>
+            <img id="image" src="https://cdn.shopify.com/s/files/1/0089/7912/0206/products/LOTFEELPJ541_21_-MO_800x.jpg?v=1638346603" width="490" height="490">
+            </div>
+        <div class="right">
+            <h2>Price</h2>
+            <p id="price">$20</p>
+         <a class="btn btn-primary" href="confirm.php" role="button">Confirm Order</a>
+        </div>
+    </div>
+    <div style="background-color:#A78C59;text-align:center;padding:10px;margin-top:7px;">© copyright JuanNickLogan.oak</div>
+  </body>
 </html>
