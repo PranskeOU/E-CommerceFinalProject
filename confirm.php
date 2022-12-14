6ef75afe1baac62eef7f4a5ddedd87923d7d3eef
Logan-Manuel <97976110+Logan-Manuel@users.noreply.github.com>
2022-12-13 20:40:35 -0600

    Update confirm.php

--- confirm.php
+++ confirm.php
@@ -1,99 +1,104 @@
 <!doctype html>
 <html lang="en">
 <head> <?php require_once("header.php"); ?>
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <style>
-     img {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
+ <title>CSS Template</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+* {
+  box-sizing: border-box;
 }
-        body {
-  /*background-image: url('https://res.cloudinary.com/teepublic/image/private/s--bxj92afB--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1564609475/production/designs/5473319_0.jpg');
-  */
-  background-repeat: no-repeat;
-  background-attachment: fixed; 
-  background-size: 100% 100%;
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
 }
-          h2 {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
+
+/* Style the header */
+header {
+  background-color: #666;
+  padding: 30px;
+  text-align: center;
+  font-size: 35px;
+  color: white;
 }
-        * {
-            box-sizing: border-box;
-        }
 
-        .menu {
-            float: left;
-            width: 20%;
-            text-align: center;
-            background: #415665;
-            height: 75px;
-            
-            border-radius: 15px;
-            padding: 20px;
-            font-size: 22px;
-            color: #fff;
-        }
+/* Create two columns/boxes that floats next to each other */
+nav {
+  float: left;
+  width: 30%;
+  height: 300px; /* only for demonstration, should be removed */
+  background: #ccc;
+  padding: 20px;
+}
 
-            .menu a {
-                background-color: dimgrey;
-                padding: 8px;
-                margin-top: 7px;
-                display: block;
-                width: 100%;
-                color: white;
-            }
+/* Style the list inside the menu */
+nav ul {
+  list-style-type: none;
+  padding: 0;
+}
 
-        .main {
-            float: left;
-            width: 60%;
-            padding: 0 20px;
-        }
+article {
+  float: left;
+  padding: 20px;
+  width: 70%;
+  background-color: #f1f1f1;
+  height: 300px; /* only for demonstration, should be removed */
+}
 
-        .right {
-            background-color: #e5e5e5;
-            float: left;
-            width: 20%;
-            padding: 15px;
-            margin-top: 7px;
-            text-align: center;
-        }
+/* Clear floats after the columns */
+section::after {
+  content: "";
+  display: table;
+  clear: both;
+}
 
-        @media only screen and (max-width: 620px) {
-            /* For mobile phones: */
-            .menu, .main, .right {
-                width: 100%;
-            }
-        }
-    </style>
+/* Style the footer */
+footer {
+  background-color: #777;
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+
+/* Responsive layout - makes the two columns/boxes stack on top of each other instead of next to each other, on small screens */
+@media (max-width: 600px) {
+  nav, article {
+    width: 100%;
+    height: auto;
+  }
+}
+</style>
 </head>
-<body style="font-family:Verdana;color:#aaaaaa;">
-    <div style="background-color:#e5e5e5;padding:15px;text-align:center;color: black;">
-        <h1>Man Clothes</h1>
-    </div>
+<body>
+
+<h2>CSS Layout Float</h2>
+<p>In this example, we have created a header, two columns/boxes and a footer. On smaller screens, the columns will stack on top of each other.</p>
+<p>Resize the browser window to see the responsive effect (you will learn more about this in our next chapter - HTML Responsive.)</p>
+
+<header>
+  <h2>Cities</h2>
+</header>
+
+<section>
+  <nav>
+    <ul>
+      <li><a href="#">London</a></li>
+      <li><a href="#">Paris</a></li>
+      <li><a href="#">Tokyo</a></li>
+    </ul>
+  </nav>
   
-<div style="overflow:auto">
-        <div class="menu">
-            <label>Enter a quantity: </label><select id="selectvalue">
-                <option>Small</option>
-                <option>Medium</option>
-                <option>Large</option>
-            </select>
+  <article>
+    <h1>London</h1>
+    <p>London is the capital city of England. It is the most populous city in the  United Kingdom, with a metropolitan area of over 13 million inhabitants.</p>
+    <p>Standing on the River Thames, London has been a major settlement for two millennia, its history going back to its founding by the Romans, who named it Londinium.</p>
+  </article>
+</section>
 
-        </div>
+<footer>
+  <p>Footer</p>
+</footer>
 
-        <div class="main">
-            <h2 id="title">Black Sweatpants</h2>
-            <img id="image" src="https://cdn.shopify.com/s/files/1/0089/7912/0206/products/LOTFEELPJ541_21_-MO_800x.jpg?v=1638346603" width="490" height="490">
-            </div>
-        <div class="right">
-            <h2>Price</h2>
-            <p id="price">$20</p>
-         <a class="btn btn-primary" href="confirm.php" role="button">Confirm Order</a>
-        </div>
-    </div>
-    <div style="background-color:#A78C59;text-align:center;padding:10px;margin-top:7px;">© copyright JuanNickLogan.oak</div>
-  </body>
+</body>
 </html>
+
