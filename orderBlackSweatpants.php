042c36007ab3ef3193d363fab5a182bb7765e828
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-14 17:16:38 -0600

    sdhsdf

--- orderBlackSweatpants.php
+++ orderBlackSweatpants.php
@@ -1,25 +1,30 @@
 <!doctype html>
 <html lang="en">
-<head> <?php require_once("header.php"); ?>
- <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<head>
+    <?php require_once("header.php"); ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
-     img {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
+        img {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
         body {
-  /*background-image: url('https://res.cloudinary.com/teepublic/image/private/s--bxj92afB--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1564609475/production/designs/5473319_0.jpg');
+            /*background-image: url('https://res.cloudinary.com/teepublic/image/private/s--bxj92afB--/t_Resized%20Artwork/c_fit,g_north_west,h_954,w_954/co_000000,e_outline:48/co_000000,e_outline:inner_fill:48/co_ffffff,e_outline:48/co_ffffff,e_outline:inner_fill:48/co_bbbbbb,e_outline:3:1000/c_mpad,g_center,h_1260,w_1260/b_rgb:eeeeee/c_limit,f_auto,h_630,q_90,w_630/v1564609475/production/designs/5473319_0.jpg');
   */
-  background-repeat: no-repeat;
-  background-attachment: fixed; 
-  background-size: 100% 100%;
-}
-          h2 {
-  display: block;
-  margin-left: auto;
-  margin-right: auto;
-}
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+            background-size: 100% 100%;
+        }
+
+        h2 {
+            display: block;
+            margin-left: auto;
+            margin-right: auto;
+        }
+
         * {
             box-sizing: border-box;
         }
@@ -30,21 +35,21 @@
             text-align: center;
             background: #415665;
             height: 75px;
-            
+
             border-radius: 15px;
             padding: 20px;
             font-size: 22px;
             color: #fff;
         }
 
-            .menu a {
-                background-color: dimgrey;
-                padding: 8px;
-                margin-top: 7px;
-                display: block;
-                width: 100%;
-                color: white;
-            }
+        .menu a {
+            background-color: dimgrey;
+            padding: 8px;
+            margin-top: 7px;
+            display: block;
+            width: 100%;
+            color: white;
+        }
 
         .main {
             float: left;
@@ -62,52 +67,62 @@
         }
 
         @media only screen and (max-width: 620px) {
+
             /* For mobile phones: */
-            .menu, .main, .right {
+            .menu,
+            .main,
+            .right {
                 width: 100%;
             }
         }
     </style>
 </head>
+
 <body style="font-family:Verdana;color:#aaaaaa;">
     <div style="background-color:#e5e5e5;padding:15px;text-align:center;color: black;">
         <h1>Man Clothes</h1>
     </div>
-  
-    <?php 
-  require_once("connection.php");
-  if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    switch ($_POST['saveType']) {
-        case 'Submit':
-            $totalPrice = 20 * $_POST['quantity'];
-            $productID = 4;
-            $sql = "insert into Order (ProductID, CustomerID, Quantity, TotalPrice) values (?,?,?,?)";
-            $stmt = $conn->prepare($sqlAdd);
-            $stmt->bind_param("iiii", $productID, $_POST['custID'], $_POST['quantity'], $totalPrice);
-            $stmt->execute();
-            echo '<div class="alert alert-success" role="alert">Message sent. Thank you for your feedback.</div>';
-        break;
+
+    <?php
+    require_once("connection.php");
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        switch ($_POST['saveType']) {
+            case 'Submit':
+                $totalPrice = 20 * $_POST['quantity'];
+                $productID = 4;
+                $sql = "insert into Order (ProductID, CustomerID, Quantity, TotalPrice) values (?,?,?,?)";
+                $stmt = $conn->prepare($sqlAdd);
+                $stmt->bind_param("iiii", $productID, $_POST['custID'], $_POST['quantity'], $totalPrice);
+                $stmt->execute();
+                echo '<div class="alert alert-success" role="alert">Message sent. Thank you for your feedback.</div>';
+                break;
         }
     }
     ?>
-<div style="overflow:auto">
-        <div class="menu">
-            <label>Customer ID: </label>
-            <input type="text" id="custID" name="custID">
-            <label>Quantity:</label>
+    <div style="overflow:auto">
+        <form action="" method="post">
+            <div class="menu">
+                <label>Customer ID: </label>
+                <input type="text" id="custID" name="custID">
+                <label>Quantity:</label>
                 <input type="text" id="quantity" name="quantity">
-        </div>
-
-        <div class="main">
-            <h2 id="title">Black Sweatpants</h2>
-            <img id="image" src="https://cdn.shopify.com/s/files/1/0089/7912/0206/products/LOTFEELPJ541_21_-MO_800x.jpg?v=1638346603" width="490" height="490">
             </div>
-        <div class="right">
-            <h2>Price</h2>
-            <p id="price">$20</p>
-         <a class="btn btn-primary" role="button" type="submit" name="saveType">Confirm Order</a>
-        </div>
+
+            <div class="main">
+                <h2 id="title">Black Sweatpants</h2>
+                <img id="image"
+                    src="https://cdn.shopify.com/s/files/1/0089/7912/0206/products/LOTFEELPJ541_21_-MO_800x.jpg?v=1638346603"
+                    width="490" height="490">
+            </div>
+            <div class="right">
+                <h2>Price</h2>
+                <p id="price">$20</p>
+                <a class="btn btn-primary" role="button" type="submit" name="saveType">Confirm Order</a>
+        </form>
     </div>
-    <div style="background-color:#A78C59;text-align:center;padding:10px;margin-top:7px;">© copyright JuanNickLogan.oak</div>
-  </body>
+    < </div>
+        <div style="background-color:#A78C59;text-align:center;padding:10px;margin-top:7px;">© copyright
+            JuanNickLogan.oak</div>
+</body>
+
 </html>
