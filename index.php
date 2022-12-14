569998b30e8842e2a5502b3987eb5e8537aa481d
PranskeOU <89473897+PranskeOU@users.noreply.github.com>
2022-12-13 19:38:02 -0600

    sdf

--- index.php
+++ index.php
@@ -113,6 +113,7 @@
 
         <script>
             function getHref(product) {
+            document.write(product);
             var href = "order" + product.replace(/\s/g, '') + ".php";
             document.getElementById('link').textContent = href;
             document.getElementById('link').innerHTML = href;
