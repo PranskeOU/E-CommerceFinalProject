0ca168bb61bf76843f18d0cdd5ec1ff562f84073
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-15 04:50:39 -0600

    Update allcustomers.php

--- allcustomers.php
+++ allcustomers.php
@@ -1,4 +1,5 @@
 <?php require_once("header.php"); ?>
+  <?php require_once("connection.php");?>
 <!doctype html>
 
 <html lang="en">
@@ -43,7 +44,6 @@
     </thead>
     <tbody>
        <?php
-   require_once("connection.php");
    $sql = "SELECT * FROM Customer";
    $result = $conn->query($sql);
 
