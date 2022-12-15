3e214e37f94552a46c5610c3e8fa9085da26ac97
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-15 04:26:30 -0600

    Update allcustomers.php

--- allcustomers.php
+++ allcustomers.php
@@ -6,6 +6,31 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Man Clothes</title>
+   <?php
+   require_once("connection.php");
+   $sql = "SELECT * FROM Customer";
+   $result = $conn->query($sql);
+
+   if ($result->num_rows > 0) {
+     // output data of each row
+     while ($row = $result->fetch_assoc()) {
+   ?>
+      <tr>
+        <td>
+          <?= $row["CustomerID"] ?>
+        </td>
+        <td>
+          <?= $row["CustomerName"] ?>
+        </td>
+        <td>
+          <?= $row["CustomerEmail"] ?>
+        </td>
+        <td>
+          <?= $row["CustomerPhone"] ?>
+        </td>
+        <td>
+          <?= $row["CustomerAddress"] ?>
+        </td>
 </head>
       
 <body> 
@@ -46,31 +71,7 @@
       </tr>
     </thead>
     <tbody>
-      <?php
-   require_once("connection.php");
-   $sql = "SELECT * FROM Customer";
-   $result = $conn->query($sql);
-
-   if ($result->num_rows > 0) {
-     // output data of each row
-     while ($row = $result->fetch_assoc()) {
-   ?>
-      <tr>
-        <td>
-          <?= $row["CustomerID"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerName"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerEmail"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerPhone"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerAddress"] ?>
-        </td>
+     
                  <td>
         <button type="button" class="btn btn-light" data-bs-toggle="modal" data-bs-target="#editcustinfo<?=$row["CustomerID"]?>">
                 Edit
