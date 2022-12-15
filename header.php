e7b7a6431be9ffd62729dfebec570af150bac554
Jescalante36 <98342374+Jescalante36@users.noreply.github.com>
2022-12-15 04:41:37 -0600

    Update header.php

--- header.php
+++ header.php
@@ -52,28 +52,4 @@
 </head>
 
 </html>
-         <?php
-   require_once("connection.php");
-   $sql = "SELECT * FROM Customer";
-   $result = $conn->query($sql);
-
-   if ($result->num_rows > 0) {
-     // output data of each row
-     while ($row = $result->fetch_assoc()) {
-   ?>
-      <tr>
-        <td>
-          <?= $row["CustomerID"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerName"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerEmail"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerPhone"] ?>
-        </td>
-        <td>
-          <?= $row["CustomerAddress"] ?>
-        </td>
+  
